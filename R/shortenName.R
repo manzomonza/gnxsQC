4522dfa4520cc4e315025176ea7fce042c2fb1dc
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-25 12:10:25 +0100

    Include match to letter before year in shorten_name

--- R/shortenName.R
+++ R/shortenName.R
@@ -9,7 +9,8 @@
 shorten_name = function(filestring){
   parsestring = dirname(filestring)
   parsestring = strsplit(parsestring, split = "/")[[1]]
-  parsestring = grep("20[2|3][0-9]", parsestring, value = TRUE)
+  parsestring = grep("[A-Z]20[2|3][0-9]", parsestring, value = TRUE)
   returnstring = stringr::str_extract(parsestring, pattern = "\\w{1,4}_.+?(?=_)")
   return(returnstring)
 }
+
